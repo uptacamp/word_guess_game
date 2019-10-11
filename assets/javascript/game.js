12bff58da3b8eb4a5f19b99c58982bd235bfda0a
Melanson <Lee.Melanson@LibertyMutual.com>
2019-10-11 12:31:24 -0400

    DoneEnough

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,18 +5,18 @@ String.prototype.setCharAt = function(idx, chr) {
 };
  
 let puzzle = {
-  input_character: "o",
-  selected_show: "The Office",
+  input_character: "",
+  selected_show: "",
   displayed_result: "",
-  unguessed_character_count: 0,
   wins: 0,
   losses: 0,
   guesses_left: 10,
-  matching_letters: 0
+  matching_letters: 0,
+  first_guess_occurred: false,
+  letters_guessed: ""
   };
 
-puzzle.input_character = puzzle.input_character.toUpperCase();
-puzzle.selected_show = puzzle.selected_show.toUpperCase();
+//puzzle.selected_show = puzzle.selected_show.toUpperCase();
 puzzle.displayed_result = puzzle.displayed_result.toUpperCase();
 
 let show_choices = [
@@ -31,61 +31,54 @@ let show_choices = [
         'Family Guy',
         'Sons Of Anarchy']
         
- 
-
-new UserInput();
-
-
-//everything starts with user input.
 
 function UserInput (){
   console.log("function: user input") ;
+  puzzle.input_character = event.keyCode;
+  puzzle.input_character = String.fromCharCode(puzzle.input_character);
+  puzzle.input_character = puzzle.input_character.toUpperCase();
+  console.log(puzzle.input_character);
+  
   if (new IsValidInput(puzzle.input_character)){
-       
-       if(puzzle.guesses_left === 0) {
-          puzzle.displayed_result = "Game Over. Press Any Letter to Play Again"
-          console.log("  sorry game is over");
-          return;
-        } 
-      
-        else if (puzzle.guesses_left == 10){
+      puzzle.letters_guessed = puzzle.input_character + ", " + puzzle.letters_guessed;
+      document.getElementById("letters_guessed").innerHTML = puzzle.letters_guessed; 
+      if (puzzle.first_guess_occurred == false){
           new SetupGame();
-          
+          new IsMatchingInput();
         }
        
-      new IsMatchingInput();
+      else {
+          new IsMatchingInput();
+        }
+
+      puzzle.first_guess_occurred = true;
       
-      if (puzzle.unguessed_character_count <= 0){
-        console.log("puzzle solved!");
-        puzzle.displayed_result = "Puzzle Solved. Press Any Key To Begin"
-      }
+      
       console.log("old displayed result: " + puzzle.displayed_result);
 
-      puzzle.guesses_left -= 1; 
+      
    
-     //insert code to display correctly guessed letter(s)
-     
-     
-     
-     var j;
+    var j;
     for (j = 0; j < puzzle.selected_show.length; j++) {
       if (puzzle.selected_show.charAt(j)== puzzle.input_character){
-       console.log(j);
-      puzzle.displayed_result = puzzle.displayed_result.setCharAt(j, puzzle.input_character);
-               
+      puzzle.displayed_result = puzzle.displayed_result.setCharAt(j, puzzle.input_character);       
       }; 
-  
-} 
-     console.log("new displayed result: " + puzzle.displayed_result);
-      
+      document.getElementById("banner").innerHTML = puzzle.displayed_result;
+      } 
+
       console.log("matching letters (this guess): " + puzzle.matching_letters);
       
-      console.log("new number of unguessed letters: " + puzzle.unguessed_character_count);
       
-      console.log("guesses left: " + puzzle.guesses_left);
-       }
-   
-  else {
+      if(puzzle.displayed_result == puzzle.selected_show){
+        
+      setTimeout(function() {
+        alert("YOU WIN! Press OK to start new game");
+        location.reload();
+      }, 500);
+     
+      }
+    }
+   else {
     //do nothing if it wasnt a valid input
     return; 
   }
@@ -95,7 +88,7 @@ function UserInput (){
 //check if entry was a letter
 function IsValidInput(input_character) {
   console.log("function: IsValidInput");
-   if(/^[a-zA-Z]/.test(input_character))
+   if(/^[a-zA-Z]/.test(input_character) & puzzle.input_character !== "")
      {
       return true;
      } 
@@ -109,35 +102,52 @@ function IsValidInput(input_character) {
 //new game orchestration
 function SetupGame (){
   console.log("function: SetupGame");
+  puzzle.selected_show= "";
+  puzzle.displayed_result= "";
+  puzzle.wins= 0;
+  puzzle.losses= 0;
+  puzzle.guesses_left= 10;
+  puzzle.matching_letters= 0;
   puzzle.selected_show = show_choices[Math.floor(Math.random() * show_choices.length)];
   puzzle.selected_show = puzzle.selected_show.toUpperCase();
   puzzle.selected_show = puzzle.selected_show.replace(/\s+/g, '');
   console.log("randomly selected show: " +  puzzle.selected_show);
-  puzzle.unguessed_character_count = puzzle.selected_show.length; 
-  
-var i;
-for (i = 0; i < puzzle.selected_show.length; i++) {
-  puzzle.displayed_result = puzzle.displayed_result + "_";
-} 
 
+  var i;
+  for (i = 0; i < puzzle.selected_show.length; i++) {
+    puzzle.displayed_result = puzzle.displayed_result + "_";
+  } 
 }
 
 //check if letter was a match or not
 function IsMatchingInput(){
   console.log("function:IsMatchingInput");
-  console.log("number of unguessed letters: " + puzzle.unguessed_character_count);    
+   
   console.log("guessed letter: " + puzzle.input_character);
   if (puzzle.selected_show.includes(puzzle.input_character.toUpperCase())){
     console.log("letter is a match");
     let n = new RegExp(puzzle.input_character.toUpperCase(), 'g' );
     puzzle.matching_letters = (puzzle.selected_show.match(n) || []).length;
-    
-    puzzle.unguessed_character_count -= puzzle.matching_letters
-    
+    console.log("guesses left: " + puzzle.guesses_left);
     return true; 
     }  
+    
   else{
     console.log("letter is not a match");
+    puzzle.guesses_left -= 1;
+    console.log("guesses left: " + puzzle.guesses_left);
+    document.getElementById("guesses_left").innerHTML = puzzle.guesses_left;
+
+    if(puzzle.guesses_left == 0) {
+      console.log("Game over");
+      
+      setTimeout(function() {
+        alert("Game Over. Click OK to load a new game");
+      location.reload();
+      }, 500);
+      
+      
+    } 
     return false;
   }
 }
